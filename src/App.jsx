7593784e0e85fbf3c46131b4d72b0573687307b4
brybryson <bryantiversonmelliza03@gmail.com>
2026-05-15 17:27:36 +0800

    changed base folder to /SolacePoint/

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import LandingPage from './LandingPage'
 import './index.css'
 
 function App() {
   return (
-    <LandingPage />
+    <Router basename="/SolacePoint">
+      <Routes>
+        {/* Redirect the root to /Home */}
+        <Route path="/" element={<Navigate to="/Home" replace />} />
+        
+        {/* The Landing Page at /Home */}
+        <Route path="/Home" element={<LandingPage />} />
+      </Routes>
+    </Router>
   )
 }
 
